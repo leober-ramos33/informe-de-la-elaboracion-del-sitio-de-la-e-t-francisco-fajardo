895474a26c84a2e602b5e8ec85b5f1300f89806f
Leober Ramos <leober.ramos33@gmail.com>
2021-05-02 14:54:30 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: process.env.NODE_ENV === "production" ? ["./public/**/*.html"] : [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
